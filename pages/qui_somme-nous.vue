9eff426b960186a7edd18d812c98568fbe000ea8
shuraDem <mathisbatailler30@gmail.com>
2023-01-25 18:46:09 +0100

    who are we MAJ

--- pages/qui_somme-nous.vue
+++ pages/qui_somme-nous.vue
@@ -1,12 +1,19 @@
 <template>
     <v-container>
-      <v-row>
-        <v-col offset="8" cols="4">
-          <h1 class="text-center text-h2 pb-5"> who are we</h1>
+      <v-row >
+        <v-col cols="12">
+          <h1 class="text-center text-h2 my-5 pb-5"> who are we</h1>
+
+        </v-col>
+        <v-col cols="5" offset="0">
           <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet congue turpis, vitae condimentum mauris rutrum eu. Praesent non eros in est placerat placerat in vel turpis. Nunc fermentum, quam ut sollicitudin rutrum, nisl dui dictum neque, vitae efficitur risus tellus sit amet nibh. Suspendisse ac arcu rhoncus, volutpat purus ac, tempor nisl. Sed vulputate at purus ut sagittis. Nunc venenatis ligula turpis, non lacinia magna lacinia vitae. Maecenas metus purus, suscipit vel volutpat quis, blandit sit amet urna. Duis interdum commodo ex, in mollis turpis sagittis eu. Duis laoreet dictum tincidunt. Pellentesque tempus erat id urna tincidunt varius. Maecenas sed lorem ligula. Donec ut quam venenatis, ornare nulla a, faucibus orci. Integer finibus nisl ac risus sodales blandit.
-              Donec laoreet leo mollis odio pulvinar fringilla. Curabitur feugiat scelerisque tortor, vel sagittis mauris dictum vitae. Integer malesuada, ligula vitae lobortis consequat, sapien massa consectetur diam, in varius massa turpis nec nisi. Integer nec justo quis dui convallis laoreet ac ac urna. Aliquam erat volutpat. Pellentesque rhoncus, felis sed semper pretium, sapien ante blandit diam, eu feugiat nunc nibh vitae nisl. Etiam quis cursus libero, id mattis arcu. Proin a metus nunc. Maecenas justo ante, venenatis et metus non, condimentum tincidunt dolor. Integer justo mauris, efficitur non lectus et, scelerisque posuere augue. Integer fringilla eros sit amet diam placerat, at pulvinar mauris dignissim. Nulla facilisi. Ut pulvinar neque et erat sollicitudin, id pulvinar sem consequat. Suspendisse nec nunc convallis erat convallis maximus vitae in libero. Proin nec nulla quis tortor ultricies euismod.
-              Morbi suscipit lorem at euismod rhoncus. In feugiat pulvinar quam. Nulla facilisi.
-            </p>
+            Donec laoreet leo mollis odio pulvinar fringilla. Curabitur feugiat scelerisque tortor, vel sagittis mauris dictum vitae. Integer malesuada, ligula vitae lobortis consequat, sapien massa consectetur diam, in varius massa turpis nec nisi. Integer nec justo quis dui convallis laoreet ac ac urna. Aliquam erat volutpat. Pellentesque rhoncus, felis sed semper pretium, sapien ante blandit diam, eu feugiat nunc nibh vitae nisl. Etiam quis cursus libero, id mattis arcu. Proin a metus nunc. Maecenas justo ante, venenatis et metus non, condimentum tincidunt dolor. Integer justo mauris, efficitur non lectus et, scelerisque posuere augue. Integer fringilla eros sit amet diam placerat, at pulvinar mauris dignissim. Nulla facilisi. Ut pulvinar neque et erat sollicitudin, id pulvinar sem consequat. Suspendisse nec nunc convallis erat convallis maximus vitae in libero. Proin nec nulla quis tortor ultricies euismod.
+            Morbi suscipit lorem at euismod rhoncus. In feugiat pulvinar quam. Nulla facilisi.
+          </p>
+        </v-col>
+        <v-col cols="5" offset="6">
+
+
             <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet congue turpis, vitae condimentum mauris rutrum eu. Praesent non eros in est placerat placerat in vel turpis. Nunc fermentum, quam ut sollicitudin rutrum, nisl dui dictum neque, vitae efficitur risus tellus sit amet nibh. Suspendisse ac arcu rhoncus, volutpat purus ac, tempor nisl. Sed vulputate at purus ut sagittis. Nunc venenatis ligula turpis, non lacinia magna lacinia vitae. Maecenas metus purus, suscipit vel volutpat quis, blandit sit amet urna. Duis interdum commodo ex, in mollis turpis sagittis eu. Duis laoreet dictum tincidunt. Pellentesque tempus erat id urna tincidunt varius. Maecenas sed lorem ligula. Donec ut quam venenatis, ornare nulla a, faucibus orci. Integer finibus nisl ac risus sodales blandit.
               Donec laoreet leo mollis odio pulvinar fringilla. Curabitur feugiat scelerisque tortor, vel sagittis mauris dictum vitae. Integer malesuada, ligula vitae lobortis consequat, sapien massa consectetur diam, in varius massa turpis nec nisi. Integer nec justo quis dui convallis laoreet ac ac urna. Aliquam erat volutpat. Pellentesque rhoncus, felis sed semper pretium, sapien ante blandit diam, eu feugiat nunc nibh vitae nisl. Etiam quis cursus libero, id mattis arcu. Proin a metus nunc. Maecenas justo ante, venenatis et metus non, condimentum tincidunt dolor. Integer justo mauris, efficitur non lectus et, scelerisque posuere augue. Integer fringilla eros sit amet diam placerat, at pulvinar mauris dignissim. Nulla facilisi. Ut pulvinar neque et erat sollicitudin, id pulvinar sem consequat. Suspendisse nec nunc convallis erat convallis maximus vitae in libero. Proin nec nulla quis tortor ultricies euismod.
               Morbi suscipit lorem at euismod rhoncus. In feugiat pulvinar quam. Nulla facilisi.
